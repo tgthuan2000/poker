266a37bb60346c03a6cb15b11f8b89141d129861
tgthuan <tgthuan2000@gmail.com>
2021-07-10 20:45:51 +0700

    build ui poker navbar

--- src/Pages/Games/NavbarGame/index.jsx
+++ src/Pages/Games/NavbarGame/index.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import './index.css'
 import { gameData } from '../../../Data'
 import { Link } from 'react-router-dom'
-import { useLocation } from 'react-router'
+import { useLocation, useParams } from 'react-router'
 
 const NavbarGame = () => {
+    const { slug } = useParams()
     const localtion = useLocation()
     const gameId = localtion.pathname.split('/')[2];
     const game = gameData.find(item => item.id === gameId)
 
     return (
-        <div className='navGame'>
-            <Link to='../../../games' className="navGame-center">
+        <div className={`navGame ${slug === 'home' ? 'active' : 'hidden'}`}>
+            <Link to='../../../games' className="navGame-left">
                 <img src={`../../../img/${game.iconImage}`} alt=" "/>
                 <span>{game.name}</span>
             </Link>
-            <Link to={`../../${gameId}`} className="navGame-left">
+            <Link to={`../../${gameId}`} className="navGame-right">
                 <i className="fas fa-arrow-left"></i>
                 <span>Room</span>
             </Link>
